48d0a3484b7e19aa3dbc504352cdf0bfef016eea
Zach Dennis <zach.dennis@gmail.com>
2018-06-07 13:20:26 -0400

    updating the welcome_spec.rb

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -5,4 +5,16 @@ describe 'Welcome page', type: :feature, js: :true do
     visit '/'
     expect(page).to have_content('Welcome to app!')
   end
+
+  it 'shows a list of welcome messages' do
+    WelcomeMessage.create!([
+      { message: 'Top of the morning to you' },
+      { message: 'Good day' }
+    ])
+
+    visit '/'
+
+    expect(page).to have_content('Top of the morning to you')
+    expect(page).to have_content('Good day')
+  end
 end
